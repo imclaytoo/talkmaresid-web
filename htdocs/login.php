809ec739c85301bb43e6f206df69077c4e60469a
erlangga231 <erlanggasetiawan231@gmail.com>
2021-06-26 21:19:48 +0700

    Update about and navigasi menu
    done semua

--- htdocs/login.php
+++ htdocs/login.php
@@ -45,7 +45,7 @@
                         </div>
                     </div>
 
-                    <li><a href="#">About</a></li>
+                    <li><a href="about.html">About</a></li>
                     <li><a href="contact.html">Contact</a></li>
                     <li><a href="login.html">Log In</a></li>
                 </ul>
--- htdocs/tampil_admin.php
+++ htdocs/tampil_admin.php
@@ -45,7 +45,7 @@
                             </div>
                         </div>
                     </div>
-                    <li><a href="#">About</a></li>
+                    <li><a href="about.html">About</a></li>
                     <li><a href="contact.html">Contact</a></li>
                     <li><a href="logout.php">Log Out</a></li>
                 </ul>
